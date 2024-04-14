9162cf4b533e61d5bb8958e430664dc9fa49ca66
raghavgpt001 <67496636+raghavgpt001@users.noreply.github.com>
2024-04-14 10:54:02 +0530

    Update page.tsx

--- apps/user-app/app/signup/page.tsx
+++ apps/user-app/app/signup/page.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from 'next-auth';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
+
 const SignupPage = async () => {
   const session = await getServerSession(authOptions);
   if (session?.user) {
